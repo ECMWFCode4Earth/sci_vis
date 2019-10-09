a735ba3815123a611341793657d2f3884bf83ade
lorenzocelli <celli.lorenzo@yahoo.it>
2019-10-09 09:25:15 +0200

    Refactoring

--- BVTK/info.py
+++ BVTK/info.py
@@ -11,10 +11,10 @@ class BVTK_NT_Info(Node, BVTK_Node):
         return []
 
     def m_connections(self):
-        return (['input'],[],[],['output'])
+        return ["Input"], [], [], ["Output"]
 
     def update_cb(self):
-        log.debug('tree updated')
+        log.debug("Tree updated.")
 
     def setup(self):
         # Make info node wider to show all text
@@ -22,33 +22,33 @@ class BVTK_NT_Info(Node, BVTK_Node):
 
     def draw_buttons(self, context, layout):
         fs = "{:.5g}"  # Format string
-        in_node, vtkobj = self.get_input_node('input')
+        in_node, vtkobj = self.get_input_node("Input")
         if not in_node:
-            layout.label('Connect a node')
+            layout.label("Connect a node")
         elif not vtkobj:
-            layout.label('Input has not vtkobj (try updating)')
+            layout.label("Input has not vtkobj (try updating)")
         else:
             vtkobj = resolve_algorithm_output(vtkobj)
             if vtkobj:
-                layout.label(text='Type: ' + vtkobj.__class__.__name__)
+                layout.label(text="Type: " + vtkobj.__class__.__name__)
 
-                layout.label(text='Points: ' + str(vtkobj.GetNumberOfPoints()))
-                if hasattr(vtkobj, 'GetNumberOfCells'):
-                    layout.label(text='Cells: ' + str(vtkobj.GetNumberOfCells()))
-                if hasattr(vtkobj, 'GetBounds'):
-                    layout.label(text='X range: ' + fs.format(vtkobj.GetBounds()[0]) +
-                                 ' - ' + fs.format(vtkobj.GetBounds()[1]))
-                    layout.label(text='Y range: ' + fs.format(vtkobj.GetBounds()[2]) +
-                                 ' - ' + fs.format(vtkobj.GetBounds()[3]))
-                    layout.label(text='Z range: ' + fs.format(vtkobj.GetBounds()[4]) +
-                                 ' - ' + fs.format(vtkobj.GetBounds()[5]))
+                layout.label(text="Points: " + str(vtkobj.GetNumberOfPoints()))
+                if hasattr(vtkobj, "GetNumberOfCells"):
+                    layout.label(text="Cells: " + str(vtkobj.GetNumberOfCells()))
+                if hasattr(vtkobj, "GetBounds"):
+                    layout.label(text="X range: " + fs.format(vtkobj.GetBounds()[0]) +
+                                 " - " + fs.format(vtkobj.GetBounds()[1]))
+                    layout.label(text="Y range: " + fs.format(vtkobj.GetBounds()[2]) +
+                                 " - " + fs.format(vtkobj.GetBounds()[3]))
+                    layout.label(text="Z range: " + fs.format(vtkobj.GetBounds()[4]) +
+                                 " - " + fs.format(vtkobj.GetBounds()[5]))
                 data = {}
-                if hasattr(vtkobj, 'GetPointData'):
-                    data['Point data '] = vtkobj.GetPointData()
-                if hasattr(vtkobj, 'GetCellData'):
-                    data['Cell data '] = vtkobj.GetCellData()
-                if hasattr(vtkobj, 'GetFieldData'):
-                    data['Field data '] = vtkobj.GetFieldData()
+                if hasattr(vtkobj, "GetPointData"):
+                    data["Point data "] = vtkobj.GetPointData()
+                if hasattr(vtkobj, "GetCellData"):
+                    data["Cell data "] = vtkobj.GetCellData()
+                if hasattr(vtkobj, "GetFieldData"):
+                    data["Field data "] = vtkobj.GetFieldData()
                 for k in data:
                     d = data[k]
                     for i in range(d.GetNumberOfArrays()):
@@ -56,8 +56,8 @@ class BVTK_NT_Info(Node, BVTK_Node):
                         r = arr.GetRange()
                         name = arr.GetName()
                         row = layout.row()
-                        row.label(text=k + '[' + str(i) + ']: \'' + name + '\': '
-                                  + fs.format(r[0]) + ' - ' + fs.format(r[1]))
+                        row.label(text=k + "[" + str(i) + "]: '" + name + "': "
+                                  + fs.format(r[0]) + " - " + fs.format(r[1]))
 
         layout.separator()
         row = layout.row()
@@ -74,13 +74,16 @@ class BVTK_NT_Info(Node, BVTK_Node):
     def apply_properties(self, vtkobj):
         pass
 
+    def apply_inputs(self, vtkobj):
+        pass
+
     def get_output(self, socket):
-        return self.get_input_node('input')[1]
+        return self.get_input_node("Input")[1]
 
 
 TYPENAMES = []
 add_class(BVTK_NT_Info)
-TYPENAMES.append('BVTK_NT_Info')
+TYPENAMES.append("BVTK_NT_Info")
 
 menu_items = [NodeItem(x) for x in TYPENAMES]
 CATEGORIES.append(BVTK_NodeCategory("Debug", "Debug", items=menu_items))
